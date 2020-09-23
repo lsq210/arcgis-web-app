aafdaa08a8c4e886c6c2376be2c1cb1832c60d68
lsq <1135595767@qq.com>
2020-09-23 23:27:38 +0800

    添加地图

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
-    <div id="app">
-      <web-map />
+  <div id="app">
+    <div>
+      <img src="@/img/taxi-icon.png" />
+      <div class="title">武汉市出租车时空轨迹分析系统</div>
     </div>
+    <web-map />
+  </div>
 </template>
 
 <script>
-import WebMap from './components/WebMap.vue';
+import WebMap from "./components/WebMap.vue";
 
 export default {
-    name: 'App',
-    components: { WebMap }
+  name: "App",
+  components: { WebMap },
 };
 </script>
 
 <style>
-html,body {
+html,
+body {
   margin: 0;
   padding: 0;
   height: 100%;
@@ -27,5 +32,19 @@ html,body {
   margin: 0;
   width: 100%;
   height: 100%;
+}
+.title {
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  /* height: 40px; */
+  z-index: 100;
+  font-size: 26px;
+  margin: 6px 0 0 50px;
+}
+img {
+  width: 30px;
+  height: 30px;
+  margin: 8px 10px 0 14px;
 }
 </style>
--- src/components/WebMap.vue
+++ src/components/WebMap.vue
@@ -3,40 +3,90 @@
 </template>
 
 <script>
-import { loadModules } from 'esri-loader';
+import { loadModules } from "esri-loader";
 
 export default {
-  name: 'web-map',
+  name: "web-map",
   mounted() {
     // lazy load the required ArcGIS API for JavaScript modules and CSS
-    loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
-    .then(([ArcGISMap, MapView]) => {
-      const map = new ArcGISMap({
-        basemap: 'topo-vector'
-      });
+    loadModules([
+      "esri/Map",
+      "esri/views/MapView",
+      "esri/layers/FeatureLayer",
+      "esri/widgets/CoordinateConversion",
+      // "esri/widgets/Directions",
+      // "esri/widgets/Bookmarks",
+      // "esri/widgets/Expand"
+      ], { css: true }).then(([
+        ArcGISMap,
+        MapView,
+        FeatureLayer,
+        CoordinateConversion,
+        // Directions,
+        // Bookmarks,
+        // Expand
+        ]) => {
+        var map = new ArcGISMap({
+          basemap: "topo-vector",
+        });
+        // var layer0 = new FeatureLayer({
+        //   url:
+        //     "https://services6.arcgis.com/iIGNxHY29Qqg0LRI/arcgis/rest/services/20140501_0h/FeatureServer",
+        // });
+        var layer0 = new FeatureLayer({
+          url:
+          "https://services6.arcgis.com/iIGNxHY29Qqg0LRI/arcgis/rest/services/10min/FeatureServer"
+        });
+        map.add(layer0, 0);
+        this.view = new MapView({
+          container: this.$el,
+          map: map,
+          center: [114.28518, 30.55068],
+          zoom: 13,
+        });
+        var ccWidget = new CoordinateConversion({
+          view: this.view
+        });
+        // var directionsWidget = new Directions({
+        //   view: this.view,
+        //   // Point the URL to a valid route service that uses an
+        //   // ArcGIS Online hosted service proxy instead of the default service
+        //   routeServiceUrl:
+        //     "https://utility.arcgis.com/usrsvcs/appservices/uz57EVPszZxo5yzm/rest/services/World/Route/NAServer/Route_World"
+        // });
+        // const bookmarks = new Bookmarks({
+        //   view: this.view,
+        //   // allows bookmarks to be added, edited, or deleted
+        //   editingEnabled: true
+        // });
 
-      this.view = new MapView({
-        container: this.$el,
-        map: map,
-        center: [114.28518, 30.55068],
-        zoom: 13
-      });
-    });
+        // const bkExpand = new Expand({
+        //   view: this.view,
+        //   content: bookmarks,
+        //   expanded: true
+        // });
+        this.view.ui.add(ccWidget, "bottom-left");
+        // this.view.ui.add(directionsWidget, {
+        //   position: "top-right"
+        // });
+        // this.view.ui.add(bkExpand, "top-right");
+      }
+    );
+    
   },
   beforeUnmount() {
     if (this.view) {
       // destroy the map view
       this.view.container = null;
     }
-  }
+  },
 };
-
 </script>
 
 <style scoped>
 div {
   padding: 0;
-  margin: 0;
+  margin-top: 0;
   width: 100%;
   height: 100%;
 }
